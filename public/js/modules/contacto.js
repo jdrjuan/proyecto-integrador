ef0d6bdfcc077a00c6052a63ef48131c5d1d856c
Juan Romero <jdrjuan@gmail.com>
2022-11-27 19:13:56 -0300

    Commit inicial

---
+++ public/js/modules/contacto.js
@@ -0,0 +1,11 @@
+
+console.log('🆗: Módulo PageContacto cargado.');
+
+class PageContacto {
+
+    static async init () {
+        console.log('PageContacto.init()');
+    }
+}
+
+export default PageContacto;
---
+++ server.js
@@ -0,0 +1,22 @@
+import express from 'express';
+import routerProducts from './router/products.js';
+// import ProductModelMongoDB from './model/products-mongodb.js';
+import config from './config.js';
+
+// ProductModelMongoDB.connectDB();
+// await ProductModelMongoDB.connectDB();
+// ProductModelMongoDB.connectDB();
+
+const app = express();
+
+app.use(express.static('public', { extensions: ['html', 'htm'] }));
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
+// Caso de uso Products
+app.use('/api/products', routerProducts);
+
+
+const PORT = config.PORT;
+const server = app.listen(PORT, () => console.log(`Servidor Express escuchando en el puerto ${PORT}.`));
+server.on('error', error => console.log('Error al iniciar el servidor Express: ' + error.message));
